4cf4c5808c9337b0c20a485a4c7db736698f785f
liangleslie <57534857+liangleslie@users.noreply.github.com>
2020-03-07 16:52:13 -0800

    Update run.sh

--- hassio-hostapd/hassio-hostapd/run.sh
+++ hassio-hostapd/hassio-hostapd/run.sh
@@ -24,10 +24,9 @@ WPA_PASSPHRASE=$(jq --raw-output ".wpa_passphrase" $CONFIG_PATH)
 CHANNEL=$(jq --raw-output ".channel" $CONFIG_PATH)
 ADDRESS=$(jq --raw-output ".address" $CONFIG_PATH)
 NETMASK=$(jq --raw-output ".netmask" $CONFIG_PATH)
-BROADCAST=$(jq --raw-output ".broadcast" $CONFIG_PATH)
 
 # Enforces required env variables
-required_vars=(SSID WPA_PASSPHRASE CHANNEL ADDRESS NETMASK BROADCAST)
+required_vars=(SSID WPA_PASSPHRASE CHANNEL ADDRESS NETMASK)
 for required_var in "${required_vars[@]}"; do
     if [[ -z ${!required_var} ]]; then
         error=1
